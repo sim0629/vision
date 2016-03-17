a83458dbda73d9370d97f35871276313349f7b45
Gyumin Sim <sim0629@gmail.com>
2016-03-18 02:31:05 +0900

    Implement Part 3

---
+++ YOURCODE/createTextonLibrary.m
@@ -0,0 +1,22 @@
+% this scripts assumes that you are in the YOURCODE directory.
+
+giantFeatureMatrix = [];
+numberOfClusters = 18;
+
+dirname = '../random';
+d = dir(dirname);
+
+for i = 1 : length(d)
+    filename = d(i).name;
+    [~, ~, ext] = fileparts(filename);
+    if strcmp(ext, '.jpg') || strcmp(ext, '.tiff')
+        img = imread(sprintf('%s/%s', dirname, filename));
+        vectors = extractResponseVectors(img);
+        [num, ~] = size(vectors);
+        sampled = datasample(vectors, ceil(num / 100));
+        giantFeatureMatrix = [giantFeatureMatrix; sampled];
+    end
+end
+
+[~, TextonLibrary] = kmeans(giantFeatureMatrix, numberOfClusters, 'EmptyAction', 'drop');
+save('TextureLibrary.mat', 'TextonLibrary')
