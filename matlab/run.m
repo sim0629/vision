6ba52c13b22f8d1128d8a2a26e0b3f74d44913f8
Gyumin Sim <sim0629@gmail.com>
2016-04-06 13:27:51 +0900

    Add writeup and some comments

--- matlab/run.m
+++ matlab/run.m
@@ -1,3 +1,14 @@
+% by Gyumin Sim
+%
+% To run this script:
+%
+% 1. The current directory should be '/matlab/'.
+% 2. There should be '/result/' directory and other image directories in
+%    the parent directory of the current directory.
+% 3. The kdtree library should be built, and
+%    the binaries and the involving '.m' files should be in
+%    '/matlab/kdtree_bin/' directory.
+
 addpath('kdtree_bin');
 addpath('..');
 
